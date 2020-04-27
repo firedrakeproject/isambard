ce69ea43abb3ce10ea0ecdb491ff479b1514ff2a
Jack Betteridge <J.D.Betteridge@bath.ac.uk>
2020-04-27 21:49:30 +0100

    Changed venv name in activate script back to firedrake

--- firedrake_activate.sh
+++ firedrake_activate.sh
@@ -19,7 +19,7 @@ module load valgrind/3.13.0
 # Set main to be working directory
 export MAIN=/home/ba-jbetteridge/firedrake_fix/
 # Include the name of the venv
-export VENV_NAME=new_script # Or wahtever you named the venv
+export VENV_NAME=firedrake # Or wahtever you named the venv
 
 # Dynamic linking
 export CRAYPE_LINK_TYPE=dynamic
